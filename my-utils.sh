3c1909781da97439a8bb90a696939f739538496a
QGrain <zhiyuzhang1999@163.com>
2020-09-03 10:15:34 +0800

    add check_copy()

--- my-utils.sh
+++ my-utils.sh
@@ -31,3 +31,24 @@ check_delete() {
         fi
     done
 }
+
+check_copy() {
+    last_str=${1: -1}
+    if [[ $last_str == '/' ]]
+    then
+        src_path=${1%?}
+    else
+        src_path=$1
+    fi
+    base_path=${src_path%/*}
+
+    if [[ -d $src_path ]]
+    then
+        create_dir $2/$base_path
+        cp -r $src_path $2/$base_path/
+    elif [[ -f $src_path ]]
+    then
+        create_dir $2/$base_path
+        cp $src_path $2/$base_path/
+    fi
+}
